5fb237601f1db0df2fd0e8aed1a60c00357a733e
Rama Deshpande <rama@deshpandez.com>
2023-09-17 19:25:58 -0400

    removed duplicate dashboard component

--- app/src/app/dash/dash.component.ts
+++ app/src/app/dash/dash.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dash',
   templateUrl: './dash.component.html',
   styleUrls: ['./dash.component.css']
 })
 export class DashComponent {
+
+  constructor(private router: Router) {
+  }
+  logout() {
+    this.router.navigate(["/login"]);
+    console.log("You've been successfully logged out.");
+  }
 
 }
