4d972434c464dc167628f2496c03354d137a2431
Rama Deshpande <rama@deshpandez.com>
2023-09-26 12:54:50 -0400

    user story 5.3, client side validation of email input

--- app/src/app/forgot-pass/forgot-pass.component.ts
+++ app/src/app/forgot-pass/forgot-pass.component.ts
@@ -11,5 +11,21 @@ export class ForgotPassComponent {
   public emailInput = '';
   reset() {
     
+    if (this.emailInput.indexOf('@') != -1) {
+      var at = this.emailInput.indexOf('@');
+      var afterAt = this.emailInput.substring(at + 1, this.emailInput.length);
+      alert("after: " + afterAt)
+      var beforeAt = this.emailInput.substring(0, at);
+      alert("before: " + beforeAt)
+      if (beforeAt.length >= 1 && afterAt.length >= 3 && afterAt.indexOf('.') != -1) {
+        //send email here @ash-bizzle
+      } else {
+        alert("Please input a valid email.");
+      }
+    }
+    else {
+      alert("Please input a valid email.");
+    }
+    
   }
 }
