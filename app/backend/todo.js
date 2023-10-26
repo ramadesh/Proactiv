092610cd40d6c2d30ae04cfc862e5e03dbdaa078
Rama Deshpande <rama@deshpandez.com>
2023-10-26 15:53:20 -0400

    todo.js backend added

---
+++ app/backend/todo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+// Define the schema for your data
+const todoSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  todo: {
+    type: String,
+    required: true
+  },
+  due: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+// Create the model using the schema
+module.exports = mongoose.model('ToDo', todoSchema);
