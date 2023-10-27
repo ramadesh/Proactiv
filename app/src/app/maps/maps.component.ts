f0072e589cb416bb8a2f0da3784007f290a58136
ZtevenShang <zhan4030@purdue.edu>
2023-10-26 22:24:36 -0400

    added global map to maps component using HERE maps api

--- app/src/app/maps/maps.component.ts
+++ app/src/app/maps/maps.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import H from '@here/maps-api-for-javascript';
+import onResize from 'simple-element-resize-detector';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css']
 })
-export class MapsComponent {
 
+export class MapsComponent {
+  private map?: H.Map;
+
+  @ViewChild('map') mapDiv?: ElementRef; 
+
+  ngAfterViewInit(): void {
+    if (!this.map && this.mapDiv) {
+      // Instantiate a platform, default layers and a map as usual.
+      const platform = new H.service.Platform({
+        apikey: 'Gp22nP47_8DVr0x_MirjZ1j_17DO750FyVbz7lQNJic'
+      });
+      const layers = platform.createDefaultLayers();
+      const map = new H.Map(
+        this.mapDiv.nativeElement,
+        // Add type assertion to the layers object... 
+        // ...to avoid any Type errors during compilation.
+        (layers as any).vector.normal.map,
+        {
+          pixelRatio: window.devicePixelRatio,
+          center: {lat: 0, lng: 0},
+          zoom: 2,
+        },
+      );
+      onResize(this.mapDiv.nativeElement, () => {
+        map.getViewPort().resize();
+      });
+      this.map = map;
+    }
+  }
 }
