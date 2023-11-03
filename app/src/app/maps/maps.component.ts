189752a3d4d3478af9b3657012eed0e638598a9d
Sriya Chundi <chundi@purdue.edu>
2023-11-03 04:14:28 -0400

    weather functionality

--- app/src/app/maps/maps.component.ts
+++ app/src/app/maps/maps.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-maps',
@@ -8,7 +7,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./maps.component.css']
 })
 
-export class MapsComponent {
+export class MapsComponent implements OnInit {
   query: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.google.com/maps/embed/v1/search?key=AIzaSyADNbpFpQH0hSdd8RH5p-cLWSzkpX6Y-0g&q=Coffee+near+me')
   constructor(private sanitizer: DomSanitizer) {
   }
@@ -19,5 +18,40 @@ export class MapsComponent {
   libraries() {
     this.query = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.google.com/maps/embed/v1/search?key=AIzaSyADNbpFpQH0hSdd8RH5p-cLWSzkpX6Y-0g&q=Libraries+near+me')
   }
+
+  weatherAPIKey = 'bef92f1fa030870057d2956f1544ee5d'
+  city = 'West Lafayette';
+  state = 'IN';
+  countryCode = 'US';
+  limit = 5
+  temperature: any
+  weatherDescription: any
+
+  async ngOnInit() {
+    
+    const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.city},${this.state},${this.countryCode}&limit=${this.limit}&appid=${this.weatherAPIKey}`, {
+        method: "GET"
+      })
+
+    const geolocation = await data.json();
+    const lat = geolocation[0].lat
+    const lon = geolocation[0].lon
+
+    console.log(lat + " " + lon)
+
+    this.getCurrentWeather(lat, lon)
+
+  }
+
+  async getCurrentWeather(lat: number, lon: number) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.weatherAPIKey}&units=imperial`)
+
+    const currentWeather = await data.json()
+    this.temperature = currentWeather.main.temp
+    this.weatherDescription = currentWeather.weather[0].description
+
+    console.log(currentWeather.weather)
+  }
+
   
 }
