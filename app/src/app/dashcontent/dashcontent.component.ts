6e92f75168cd8860481deb3f812a994edc63373b
Rama Deshpande <rama@deshpandez.com>
2023-12-05 15:37:13 -0500

    not hardcoded username anymore for journal entries

--- app/src/app/dashcontent/dashcontent.component.ts
+++ app/src/app/dashcontent/dashcontent.component.ts
@@ -22,7 +22,7 @@ export class PastjournalsComponent {
   public journals: any = [];
   public tasks: any = [];
   public spotifyProfilePic = this.data.spotifyProfilePic;
-  
+
   profile : Profile = {
     displayName: '',
     userId : '',
@@ -40,8 +40,7 @@ export class PastjournalsComponent {
   longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
   
   getJournals() {
-    //let params = new HttpParams().set('userId', this.userID!);
-    let params = new HttpParams().set('userId', "ashsproactivtest");
+    let params = new HttpParams().set('userId', this.userID!);
     this.http.get('http://localhost:5002/dashjournals', { params })
     .subscribe(response => {
       console.log(response);
